4efcce1fc37002f06c05453164d385ce6b4329fb
Steve <studiouifxdesignersandcoders@gmail.com>
2021-04-01 18:41:12 -0400

    Add project files.

---
+++ EggSaver/Form1.Designer.cs
@@ -0,0 +1,68 @@
+﻿
+namespace EggSaver
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.Black;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Font = new System.Drawing.Font("Sitka Small", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.ForeColor = System.Drawing.SystemColors.Menu;
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(776, 426);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Init);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox textBox1;
+    }
+}
+
---
+++ EggSaver/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EggSaver
+{
+    public partial class Form1 : Form
+    {
+        public string s1 = "The E E G is coming...";
+        public string s2 = "What does it say?";
+        public string s3 = "The E E G speaks of love and forgiveness of humankind.";
+        public string s4 = "E E G is fren...";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public async void Init(object sender, EventArgs e)
+        {
+            await Typewrite(s1);
+            await Typewrite(s2);
+            await Typewrite(s3);
+            await Typewrite(s4);
+            while (textBox1.Text != string.Empty)
+            {
+                await Task.Delay(10);
+                textBox1.Text = textBox1.Text.Remove(textBox1.Text.Length - 1);
+            }
+            PrintEgg();
+        }
+        public async void PrintEgg()
+        {
+            while(true)
+            {
+                await Task.Delay(10000);
+                textBox1.ForeColor = Egg.BuildColors()[new Random().Next(0, Egg.BuildColors().Length)];
+                textBox1.Text += "E E G -- 🥚 ";
+            }
+        }
+        public void Indent()
+        {
+            textBox1.Text += "\n";
+        }
+        public async Task Typewrite(string ttp)
+        {
+            foreach (char c in ttp)
+            {
+                await Task.Delay(10);
+                textBox1.Text += c;
+            }
+            await Task.Delay(2000);
+            Indent();
+        }
+    }
+    class Egg
+    {
+        public static string[] BuildEgge()
+        {
+            List<string> egge = new List<string>();
+            egge.Add(" ********   ********     ******** ");
+            egge.Add("/**/////   /**/////     **//////**");
+            egge.Add("/**        /**         **      // ");
+            egge.Add("/*******   /*******   /**         ");
+            egge.Add("/**////    /**////    /**    *****");
+            egge.Add("/**        /**        //**  ////**");
+            egge.Add("/********  /********   //******** ");
+            egge.Add("////////   ////////     ////////  ");
+            return egge.ToArray();
+        }
+        public static Color[] BuildColors()
+        {
+            List<Color> tocl = new List<Color>();
+            tocl.Add(Color.Red);
+            tocl.Add(Color.Blue);
+            tocl.Add(Color.Yellow);
+            tocl.Add(Color.White);
+            tocl.Add(Color.Cyan);
+            tocl.Add(Color.HotPink);
+            tocl.Add(Color.Aqua);
+            tocl.Add(Color.Chocolate);
+            tocl.Add(Color.Orange);
+            tocl.Add(Color.Purple);
+            return tocl.ToArray();
+        }
+    }
+}
